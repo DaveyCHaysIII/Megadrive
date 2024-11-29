9ea0d3491ded5819c4fdff475e6677b8deff3cef
Davey C Hays III <david.c.hays.iii@gmail.com>
2024-11-29 19:32:38 +0000

    taking a break for lunch

--- ttt/main.cpp
+++ ttt/main.cpp
@@ -1,42 +1,47 @@
 #include "main.h"
 
+//---------------------------------------Main function
+
 int main(int argc, char **argv, char **env) {
 	SetTraceLogLevel(LOG_ERROR);
 	// Initialize the window
 	InitWindow(1280, 800, "Raylib Game");
-	Gamestate state;
-	init_game(argc, argv, &state);
-	Vector2 mouse_coords_txt = DEFAULT_POSITION;
-	bool curtainActive = false;
-	bool fadeActive = false;
-	// Main game loop
+
+	//--------------------------Init
+	int *current_game_state;
+	Color *colors;
+	Texture2d *textures;
+	double framecount;
+	//look into how audio works?
+
+	init(argc, argv, current_game_state, colors, textures);
+
 	while (!WindowShouldClose())
 	{
-	//-------------- Draw space
-	BeginDrawing();
-	ClearBackground(state.colors.window);
-	SetTargetFPS(TARGET_FPS);
-	DrawText("MEGADrive!", 10, 10, 30, state.colors.window_txt_fore);
+		framecount = getFrames();
+		//-------------------------Draw space
+		BeginDrawing();
+		ClearBackground(colors[WINDOW_BACKGROUND]);
+		DrawText("MEGADrive!", LOGO_POSX, LOGO_POSY, LOGO_SIZE, colors[WINDOW_TEXT]);
+		DrawRectangle(GAME_X, GAME_Y, GAME_WIDTH, GAME_HEIGHT, colors[BACKGROUND]);
+		//-------------------------Game Area
+		if (current_game_state = SPLASH)
+			splash();
+		else if (current_game_state = GAME_PLAYING)
+			game_playing();
+		else if (current_game_state = GAME_OVER)
+			game_over();
+		else if (current_game_state = SCORE_BOARD)
+			scoreBoard();
 
-	//game area
-	DrawRectangle(240, 100, 800, 600, state.colors.game);
-	DrawText("Howdy Ho!", 630, 280, 20, GREEN);
-	if (curtainActive)
-		drawCurtains(240, 100, 600, 800, BLUE, 8);
-	if (fadeActive)
-		fadeToBlack(240, 100, 600, 800, 8);
+		//-------------------------Close
 
-	display_mouse_coords(mouse_coords_txt, 20, state.colors.window_txt_back);
-	EndDrawing();
-	//--------------
+		display_mouse_coords(DEFAULT_POSITION, 20, colors[WINDOW_TEXT]);
+		EndDrawing();
+		//-------------------------End
 
-	if (IsKeyPressed(KEY_ESCAPE))
-		break;
-	if (IsKeyPressed(KEY_D))
-		curtainActive = true;
-	if (IsKeyPressed(KEY_F))
-		fadeActive = true;
-	state.frameCount++;
+		if (IsKeyPressed(KEY_ESCAPE))
+			break;
 	}
 
 	// Close the window
@@ -44,6 +49,72 @@ int main(int argc, char **argv, char **env) {
 	return 0;
 }
 
+
+//----------------------------------------- Game Specific Functions
+
+void init_game(int argc, char **argv, int *state, Color *gameColors, Texture2d *textures)
+{
+
+	if (argc < 2)
+	{
+		const char *asset_fp = ""; //filepath to assets directory
+		state = SPLASH;
+		gameColors =
+		[
+			GRAY,
+			LIGHTGRAY,
+			DARKGRAY,
+			GRAY,
+			BLACK,
+			GREEN
+		];
+		textures =
+		[
+			LoadTexture(),
+			LoadTexture(),
+			//etc
+		];
+
+
+
+	}
+	/*else
+	{
+		//argv[0] - game state 0
+		//argv[1] - game state 1
+		//argv[2] - game state 2
+	}*/
+}
+
+void splash(int *state)
+{
+	//insert splash logic here
+
+	state = GAME_PLAYING;
+};
+void game_playing(int *state)
+{
+	//insert game_playing logic here
+};
+
+void game_over()
+{
+	//instert game_over logic here
+};
+
+void scoreBoard()
+{
+	//insert scoreboard logic here
+};
+
+//-----------------------------------Begin library candidate Functions
+
+double getFrames()
+{
+	static double frames = 0;
+	frames++;
+	return frames;
+}
 
 void display_mouse_coords(Vector2 text_position, int text_size, Color text_color)
 {
@@ -64,48 +135,27 @@ void display_mouse_coords(Vector2 text_position, int text_size, Color text_color
 			text_color);
 }
 
-void init_game(int argc, char **argv, Gamestate *state)
+int timer(int seconds, double framecount, int target_fps)
 {
-
-	if (argc < 2)
-	{
-		state->colors = palettes[0];
-		state->music = (Music){ 0 };
-		state->soundEffects = NULL;
-		state->textures = NULL;
-		state->soundCount = 0;
-		state->textureCount = 0;
-		state->frameCount = 0; //ALWAYS init to 0
-	}
-	/*else
-	{
-		//argv[0] - game state 0
-		//argv[1] - game state 1
-		//argv[2] - game state 2
-	}*/
-}
-
-int timer(double seconds, Gamestate *state)
-{
-	double time = (seconds * TARGET_FPS);
-	if (time < (seconds * TARGET_FPS) + 1)
-		time += state->frameCount;
-	if (time <= state->frameCount)
+	double time = (seconds * target_fps);
+	if (time < (seconds * target_fps) + 1)
+		time += frameCount;
+	if (time <= frameCount)
 		return (1);
 	return (0);
 }
 
-int countDownTimer(int seconds, Gamestate *state)
+int countDownTimer(int seconds, double frameCount, int target_fps)
 {
 	static int countDownSecs = 0;
 	static double targetFrames = 0;
 
-	if (targetFrames = 0)
+	if (targetFrames == 0)
 	{
 		countDownSecs = seconds;
-		targetFrames = state->frameCount + (seconds * TARGET_FPS);
+		targetFrames = frameCount + (seconds * target_fps);
 	}
-	if (state->frameCount >= targetFrames - (countDownSecs * TARGET_FPS))
+	if (frameCount >= targetFrames - (countDownSecs * target_fps))
 	{
 		countDownSecs--;
 	}
@@ -123,8 +173,10 @@ int formatTimer(int timeInSeconds, char *buffer)
 	return 0;
 }
 
-void drawCurtains(int posX, int posY, int height, int width, Color color, float speed)
+int drawCurtains(int posX, int posY, int height, int width, Color color, int speed, int direction)
 {
+	//todo: implement directionality
+	(void)direction;
 	static int curtain_height = 0;
 	int curtain_width = width;
 	color.a = 128;
@@ -133,17 +185,39 @@ void drawCurtains(int posX, int posY, int height, int width, Color color, float 
 	if (curtain_height < height)
 	{
 		curtain_height += 1 + speed;
+		return (0);
 	}
+	return (1);
 }
 
-void fadeToBlack(int posX, int posY, int height, int width, int speed)
+int fadeToBlack(int posX, int posY, int height, int width, int speed)
 {
 	static unsigned char fade = 0;
 
 	Color color = { 0, 0, 0, fade };
 	DrawRectangle(posX, posY, width, height, color);
-	if ((fade + 1 + speed) <= 255)
-		fade += 1 + speed;
+	if (fade == 255)
+		return 1
+
+	if ((fade + (1 + speed)) <= 255)
+		fade += (1 + speed);
 	else
 		fade = 255;
+	return (0);
+}
+
+int fadeFromBlack(int posX, int posY, int height, int width, int speed)
+{
+	static unsigned char fade = 255;
+
+	Color color = { 0, 0, 0, fade };
+	DrawRectangle(posX, posY, width, height, color);
+	if (fade == 0)
+		return 1
+
+	if ((fade - (1 + speed)) >= 0)
+		fade -= (1 + speed);
+	else
+		fade = 0;
+	return (0);
 }
--- ttt/main.h
+++ ttt/main.h
@@ -8,53 +8,73 @@
 #include <iostream>
 //------defines
 #define DEFAULT_POSITION ((Vector2){ 1200.0f, 740.0f })
-#define PALETTE_COUNT 3
 #define MAX_SOUNDS 5
 #define MAX_TEXTURES 10
 #define TARGET_FPS 60
-
+#define LOGO_POSX 10
+#define LOGO_POSY 10
+#define LOGO_SIZE 30
+#define GAME_X 240
+#define GAME_Y 100
+#define GAME_HEIGHT 600
+#define GAME_WIDTH 800
+#define SET_FLAG(flags, flag) ((flags).all_flags |= (flag))
+#define CLEAR_FLAG(flags, flag) ((flags).all_flags &= ~(flag))
+#define CHECK_FLAG(flags, flag) ((flags).all_flags & (flag))
+#define TOGGLE_FLAG(flags, flag) ((flags) ^= (flag))
 //------structs
 
-typedef struct
+
+enum Gamestate
 {
-	Color window;
-	Color game;
-	Color window_txt_fore;
-	Color window_txt_back;
-	Color game_txt_fore;
-	Color game_txt_back;
-} Palette;
-
-typedef struct
-{
-	Palette colors;
-	Music music;
-
-	Sound *soundEffects;
-	int soundCount;
-
-	Texture2D *textures;
-	int textureCount;
-
-	int frameCount;
-
-} Gamestate;
-
-
-static const Palette palettes[PALETTE_COUNT] =
-{
-	{ DARKGRAY, GRAY, WHITE, LIGHTGRAY, RAYWHITE, BLACK },
-	{ DARKBLUE, GRAY, WHITE, LIGHTGRAY, RAYWHITE, BLACK },
-	{ PINK, PURPLE, WHITE, LIGHTGRAY, RAYWHITE, BLACK }
+	SPLASH,
+	GAME_PLAYING,
+	GAME_OVER,
+	SCORE_BOARD
 };
+
+enum Directions
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
+typedef struct Flags
+{
+	unsigned int all_flags;
+
+	enum
+	{
+		FLAG_ONE   = 1 << 0,
+		FLAG_TWO   = 1 << 1,
+		FLAG_THREE = 1 << 2,
+		FLAG_FOUR  = 1 << 3
+	}
+}
+
+enum color_list
+{
+	BACKGROUND,
+	FOREGROUND,
+	BACKGROUND_TEXT,
+	FOREGROUND_TEXT,
+	WINDOW_BACKGROUND,
+	WINDOW_TEXT
+}
+
+
 
 //------funcs
 
+void init(int, char**, int *, Color *, Texture2d *)
 void display_mouse_coords(Vector2, int, Color);
-void init_game(int argc, char **argv, Gamestate *state);
-int timer(double, Gamestate *);
-int countDownTimer(int, Gamestate *);
+double getFrames(void);
+int timer(int, double, int);
+int countDownTimer(int, double, int);
 int formatTimer(int, char *);
-void drawCurtains(int, int, int, int, Color, float);
-void fadeToBlack(int, int, int, int, int);
+int drawCurtains(int, int, int, int, Color, int);
+int fadeToBlack(int, int, int, int, int);
+int fadeFromBlack(int, int, int, int, int);
 #endif
