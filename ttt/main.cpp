fdc374eb983eee2bd76101e201e83fa69d4de5e7
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-12-05 12:35:44 -0600

    current work

--- ttt/main.cpp
+++ ttt/main.cpp
@@ -8,37 +8,34 @@ int main(int argc, char **argv, char **env) {
 	InitWindow(1280, 800, "Raylib Game");
 	InitAudioDevice();
 	//--------------------------Init
-	Gamestate current_game_state;
-	Color colors[MAX_COLORS];
-	Texture2D textures[MAX_TEXTURES];
-	double frameCount;
+	Gamestate state;
 	SetTargetFPS(TARGET_FPS);
 
-	init_game(argc, argv, &current_game_state, colors, textures);
+	init_game(argc, argv, &state);
 
 	while (!WindowShouldClose())
 	{
-		frameCount = getFrames();
+		state.frameCount = getFrames();
 		//-------------------------Draw space
 		BeginDrawing();
-		ClearBackground(colors[WINDOW_BACKGROUND]);
-		DrawText("MEGADrive!", LOGO_POSX, LOGO_POSY, LOGO_SIZE, colors[WINDOW_TEXT]);
-		DrawRectangle(GAME_X, GAME_Y, GAME_WIDTH, GAME_HEIGHT, colors[BACKGROUND]);
+		ClearBackground(state.colors[WINDOW_BACKGROUND]);
+		DrawText("MEGADrive!", LOGO_POSX, LOGO_POSY, LOGO_SIZE, state.colors[WINDOW_TEXT]);
+		DrawRectangle(GAME_X, GAME_Y, GAME_WIDTH, GAME_HEIGHT, state.colors[BACKGROUND]);
 		//-------------------------Game Area
-		if (current_game_state == SPLASH)
-			splash(&current_game_state, colors, textures, frameCount);
-		else if (current_game_state == GAME_PLAYING)
-			game_playing(&current_game_state, colors, textures, frameCount);
-		else if (current_game_state == GAME_OVER)
-			game_over(&current_game_state, colors, textures, frameCount);
-		else if (current_game_state == SCORE_BOARD)
-			scoreBoard(&current_game_state, colors, textures, frameCount);
-		else if (current_game_state == END)
+		if (state.gameState == SPLASH)
+			splash(&state);
+		else if (state.gameState == GAME_PLAYING)
+			game_playing(&state);
+		else if (state.gameState == GAME_OVER)
+			game_over(&state);
+		else if (state.gameState == SCORE_BOARD)
+			scoreBoard(&state);
+		else if (state.gameState == END)
 			break;
 
 		//-------------------------Close
 
-		display_mouse_coords(DEFAULT_POSITION, 20, colors[WINDOW_TEXT]);
+		display_mouse_coords(DEFAULT_POSITION, 20, state.colors[WINDOW_TEXT]);
 		EndDrawing();
 		//-------------------------End
 
@@ -54,29 +51,29 @@ int main(int argc, char **argv, char **env) {
 
 //----------------------------------------- Game Specific Functions
 
-void init_game(int argc, char **argv, Gamestate *state, Color *gameColors, Texture2D *textures)
+void init_game(int argc, char **argv, Gamestate *state)
 {
-	//static std::string BASE_PATH = "./assets/";
-
 	if (argc < 2)
 	{
-		//BASE_PATH = "./assets/"; //filepath to assets directory
-		*state = SPLASH;
+		state->gameState = SPLASH;
 
-		gameColors[BACKGROUND] = GRAY;
-		gameColors[FOREGROUND] = LIGHTGRAY;
-		gameColors[BACKGROUND_TEXT] = DARKGRAY;
-		gameColors[FOREGROUND_TEXT] = GRAY;
-		gameColors[WINDOW_BACKGROUND] = BLACK;
-		gameColors[WINDOW_TEXT] = GREEN;
+		state->colors[BACKGROUND] = GRAY;
+		state->colors[FOREGROUND] = LIGHTGRAY;
+		state->colors[BACKGROUND_TEXT] = DARKGRAY;
+		state->colors[FOREGROUND_TEXT] = GRAY;
+		state->colors[WINDOW_BACKGROUND] = BLACK;
+		state->colors[WINDOW_TEXT] = GREEN;
 		
 
-		textures[0] = LoadTexture("./assets/ttt_spritesheet.png");
-		textures[1] = LoadTexture("./assets/ttt_marquissheet.png");
-		/*
-		sounds[0] = LoadSoundFromPath(sound1Path, BASE_PATH);
-		sounds[1] = LoadSoundFromPath(sound2Path, BASE_PATH);
-		*/
+		state->textures[MARQUIS] = LoadTexture("./assets/ttt_marquis.png");
+		state->textures[SPRITES] = LoadTexture("./assets/ttt_sprites.png");
+
+		state->rectangles[MARQUIS_SOURCE] = { 0.0f, 0.0f, 64.0f, 16.0f };
+		state->rectangles[PLAYER_X] = { 0.0f, 0.0f, 16.0f, 16.0f }; 	
+		state->rectangles[PLAYER_O] = { 16.0f, 0.0f, 16.0f, 16.0f };
+
+		init_timers(state->timers, MAX_TIMERS, TARGET_FPS);
+		state->frameCount = 0;
 	}
 	/*else
 	{
@@ -90,29 +87,42 @@ void init_game(int argc, char **argv, Gamestate *state, Color *gameColors, Textu
 	}
 }
 
-void splash(Gamestate *state, Color *colors, Texture2D *textures, double frameCount)
+void init_timers (Timer *timers, int num_timers, int fps)
+{
+	for (int i = 0; i < num_timers; i++)
+	{
+		timers[i].frames = 0;
+		timers[i].is_complete = 0;
+	}
+
+}
+
+void splash(Gamestate *state)
 {
 	//insert splash logic here
 	Rectangle marquis_dest = { 340.0f, 200.0f, 600.0f, 150.0f };
-	Rectangle marquis_source = { 0.0f, 0.0f, 64.0f, 16.0f }; 
+	Rectangle marquis_source = state->rectangles[MARQUIS_SOURCE];
+	Texture2D texture = state->textures[MARQUIS];
 
-	DrawText("Press [SPACE] to play!", 465, 550, 30, colors[BACKGROUND_TEXT]);
-	DrawTexturePro(textures[1], marquis_source, marquis_dest, (Vector2){ 0, 0}, 0, WHITE);
+	DrawText("Press [SPACE] to play!", 465, 550, 30, state->colors[BACKGROUND_TEXT]);
+	DrawTexturePro(texture, marquis_source, marquis_dest, (Vector2){ 0, 0}, 0, WHITE);
 	if (IsKeyPressed(KEY_SPACE))
-		*state = GAME_PLAYING;
+		state->gameState = GAME_PLAYING;
 };
-void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double frameCount)
+
+
+void game_playing(Gamestate *state)
 {
 	int i;
+	double frameCount = state->frameCount;
 	char buffer[50];
 	char turn_text[8];
 	Rectangle recs[9];
-	Rectangle player_x = { 0.0f, 0.0f, 16.0f, 16.0f };
-	Rectangle player_o = { 16.0f, 0.0f, 16.0f, 16.0f };
-
-	static int textured[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	static int turn = TURN_X;
+	static int turn = 1;
 	static int num_turns = 1;
+	static int textured[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	//------------------------------------------------Board generation
 	const int PADDING = 10;
 	const int OFFSET = 150;	
 	int row_1_x = BOARD_POSX;
@@ -133,11 +143,10 @@ void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double f
 	recs[7] = (Rectangle){ (float)row_2_x, (float)col_3_y, (float)BLOCK_HEIGHT, (float)BLOCK_WIDTH };
 	recs[8] = (Rectangle){ (float)row_3_x, (float)col_3_y, (float)BLOCK_HEIGHT, (float)BLOCK_WIDTH };
 
-	//insert game_playing logic here
-	//drop shadow
 	DrawRectangle(BOARD_POSX + PADDING, BOARD_POSY + PADDING, BOARD_HEIGHT + PADDING, BOARD_WIDTH + PADDING, BLACK);
-	//board
+
 	DrawRectangle(BOARD_POSX - PADDING, BOARD_POSY - PADDING, BOARD_HEIGHT + (PADDING * 2), BOARD_WIDTH + (PADDING * 2), DARKGRAY);
+	
 	for(i = 0; i <= 8; i++)
 	{
 		int start_x = 50;
@@ -150,15 +159,15 @@ void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double f
 		if (textured[i] == 1)
 		{
 			//DrawRectangleRec(recs[i], WHITE);
-			DrawTexturePro(textures[0], player_x, recs[i], (Vector2){ 0, 0}, 0, WHITE);
+			DrawTexturePro(state->textures[SPRITES], state->rectangles[PLAYER_X], recs[i], (Vector2){ 0, 0}, 0, WHITE);
 		}
-		else if (textured[i] == 2)
+		if (textured[i] == 2)
 		{
-			DrawTexturePro(textures[0], player_o, recs[i], (Vector2){ 0, 0}, 0, WHITE);
+			DrawTexturePro(state->textures[SPRITES], state->rectangles[PLAYER_O], recs[i], (Vector2){ 0, 0}, 0, WHITE);
 		};
 	};
 
-	
+	// ------------------------------------------------------------Game Logic	
 	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && turn == TURN_X) {
             Vector2 mousePos = GetMousePosition();
 
@@ -175,8 +184,8 @@ void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double f
 	if (turn == TURN_O && num_turns <= 9)
 	{
 		DrawText(turn_text, 620, 70, 30, RED);
-		int new_timer = countDownTimer(3, frameCount, TARGET_FPS);
-		if(new_timer == 0)
+		Timer turn_timer = state->timers[0];
+		if(startTimer(&turn_timer, 2, frameCount, TARGET_FPS) == 0)
 		{
 			for (i = 0; i <= 8; i++)
 			{
@@ -188,6 +197,7 @@ void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double f
 					break;
 				}
 			};
+			resetTimer(&state->timers[0]);
 		};
 	};
 	if (turn == TURN_X && num_turns <= 9) {
@@ -196,18 +206,19 @@ void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double f
 
 	if (num_turns > 9)
 	{
-		static int gate[2] = { 0, 0 };
 		//draw!
 		if(drawCurtains(GAME_X, GAME_Y, GAME_HEIGHT, GAME_WIDTH, BLUE, 8) == 0)
 		{
 			DrawText("X Score: ", 580, 250, 30, GREEN);
-			if(countDownTimer(1, frameCount, TARGET_FPS) == 0 || gate[0] == 1)
+			Timer timer1 = state->timers[1];
+			Timer timer2 = state->timers[2];
+
+
+			if(startTimer(&timer1, 1, frameCount, TARGET_FPS) == 0)
 			{
-				gate[0] = 1;
 				DrawText("Y Score: ", 580, 400, 30, GREEN);
-				if(countDownTimer(1, frameCount, TARGET_FPS) == 0 || gate[1] == 1)
+				if(startTimer(&timer2, 1, frameCount, TARGET_FPS) == 0)
 				{
-					gate[1] = 1;
 					DrawText("Press Space!", 550, 550, 30,  GREEN);
 				}
 			}
@@ -215,30 +226,31 @@ void game_playing(Gamestate *state, Color *colors, Texture2D *textures, double f
 		}
 		if (IsKeyPressed(KEY_SPACE))
 		{
+			init_timers(state->timers, MAX_TIMERS, TARGET_FPS);
 			if(fadeToBlack(1) == 0)
-				*state = GAME_OVER;
+				state->gameState = GAME_OVER;
 		}
 		return;
 	};
 };
 
-void game_over(Gamestate *state, Color *colors, Texture2D *textures, double frameCount)
+void game_over(Gamestate *state)
 {
 	//insert game_over logic here
 	fadeFromBlack(1);
-	DrawText("GAME OVER!", 620, 400, 20, colors[BACKGROUND_TEXT]);
+	DrawText("GAME OVER!", 620, 400, 20, state->colors[BACKGROUND_TEXT]);
 	if (IsKeyPressed(KEY_SPACE))
-		*state = SCORE_BOARD;
+		state->gameState = SCORE_BOARD;
 	if (IsKeyPressed(KEY_G))
-		*state = GAME_PLAYING;
+		state->gameState = GAME_PLAYING;
 };
 
-void scoreBoard(Gamestate *state, Color *colors, Texture2D *textures, double frameCount)
+void scoreBoard(Gamestate *state)
 {
 	//insert scoreboard logic here
-	DrawText("SCORE BOARD!", 620, 400, 20, colors[BACKGROUND_TEXT]);
+	DrawText("SCORE BOARD!", 620, 400, 20, state->colors[BACKGROUND_TEXT]);
 	if (IsKeyPressed(KEY_SPACE))
-		*state = END;
+		state->gameState = END;
 };
 
 //-----------------------------------Begin library candidate Functions
@@ -269,40 +281,26 @@ void display_mouse_coords(Vector2 text_position, int text_size, Color text_color
 			text_color);
 };
 
-int timer(int seconds, double frameCount, int target_fps)
+int startTimer(Timer *timer, int seconds, double frameCount, int fps)
 {
-	static double time = -1;
-	if (time = -1)
-		time = (seconds * target_fps) + frameCount;
-	if (time <= frameCount)
-	{
-		time = -1;
+	if (timer->is_complete == 1)
 		return (1);
+	if (timer->frames == 0)
+		timer->frames = frameCount + (seconds * fps);
+	if (timer->frames <= frameCount)
+	{
+		timer->is_complete = 1;
+		return (0);
 	}
-	return (0);
-};
+	return (1);
+}
 
-int countDownTimer(int seconds, double frameCount, int target_fps)
+void resetTimer(Timer *timer)
 {
-	static int countDownSecs = 0;
-	static double targetFrames = 0;
+	timer->frames = 0;
+	timer->is_complete = 0;
+}
 
-	if (targetFrames == 0)
-	{
-		countDownSecs = seconds;
-		targetFrames = frameCount + (seconds * target_fps);
-	}
-	if (frameCount >= targetFrames - (countDownSecs * target_fps))
-	{
-		countDownSecs--;
-	}
-	if (countDownSecs == 0)
-	{
-		countDownSecs = 0;
-		targetFrames = 0;
-	}
-	return countDownSecs;
-};
 
 int formatTimer(int timeInSeconds, char *buffer)
 {
