175db3b86236b289662bfc3e5cdbe5bdec5323d9
LRWyatt801 <loganwyatt1995@gmail.com>
2024-12-12 09:36:41 -0600

    Commiting updated brick_breaker

---
+++ brick_breaker/bbheader.hpp
@@ -0,0 +1,36 @@
+#ifndef BBHEADER_HPP_
+#define BBHEADER_HPP_
+
+/***** LIBRARIES ******/
+
+#include <raylib.h>
+
+namespace brick_breaker {
+class Screen_Draw {
+public:
+	char *game_title;
+	float screen_width, screen_height;
+	float viewport_width, viewport_height;
+	Vector2 viewportCoord;
+
+	Screen_Draw(char *title, float screen_w, float screen_h, float viewport_w, float viewport_h)
+	: game_title(title),
+	  screen_width(screen_w),
+	  screen_height(screen_h),
+	  viewport_width(viewport_w),
+	  viewport_height(viewport_h),
+	  viewportCoord{(screen_width - viewport_width) / 2, (screen_height - viewport_width) / 2} {}
+
+	void Init() {
+		InitWindow(screen_width, screen_height, game_title);
+	}
+
+	void Scissor() {
+
+	}
+
+}; // Screen_Draw
+
+} // namespace brick_breaker
+
+#endif // BBHEADER_HPP_
---
+++ brick_breaker/block_breaker.cpp
@@ -0,0 +1,15 @@
+#include "bbheader.hpp"
+#include <raylib.h>
+
+int main (int argc, char **argv) {
+    InitWindow(1250, 800, "Brick Breaker");
+
+    while (!WindowShouldClose()) {
+        // updates
+
+        // Drawing
+
+    }
+    CloseWindow();
+    return (0);
+}
